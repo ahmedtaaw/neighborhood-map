83d52b18d914a6529a359d40f50aa27ca9ebf474
ahmedtaaw <ahmedtaaw@gmail.com>
2018-02-28 20:08:35 +0200

    fix marker

--- js/app.js
+++ js/app.js
@@ -1,35 +1,40 @@
-var mapAPIKey='AIzaSyBhHOx6xieDMoLhIpTEF1R26ldB9F6Hrcc';
+var mapAPIKey = 'AIzaSyBhHOx6xieDMoLhIpTEF1R26ldB9F6Hrcc';
 
 var map;
 var marker;
+var infowindow;
 function initialize() {
-  var startmap = {lat: 29.9792971, lng: 31.1332649}
+  var startmap = { lat: 29.9792971, lng: 31.1332649 }
   map = new google.maps.Map(document.getElementById('map'), {
     center: startmap,
     zoom: 6
   });
- 
 
-  for (var x in landmarkslocations){
+
+  for (var x in landmarkslocations) {
     var building = landmarkslocations[x];
-    var location = new google.maps.LatLng(building.lat,building.lng);
-    addMarker(map,building.name,location);
-   
-  }
-} 
+    var location = new google.maps.LatLng(building.lat, building.lng);
+    addMarker(map, building.name, location);
 
-function addMarker(map,name,location){
+  }
+}
+
+function addMarker(map, name, location) {
   var marker = new google.maps.Marker({
-    position:location,
+    position: location,
     map: map
   });
-  var infowindow=new google.maps.InfoWindow({
-    content: name
-  });
 
-  google.maps.event.addListener(marker,'click',function(){
-    infowindow.open(map,marker);
+
+  google.maps.event.addListener(marker, 'click', function () {
+    if (typeof infowindow != 'undefined') {
+      infowindow.close();
+    }
+     infowindow = new google.maps.InfoWindow({
+      content: name
+    });
+    infowindow.open(map, marker);
   });
 }
 
-google.maps.event.addDomListener(window,'load',initialize);
+google.maps.event.addDomListener(window, 'load', initialize);
