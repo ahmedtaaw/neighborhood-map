d65446c6a622070a0ad2f3de91f9d2bdab239f0a
ahmedtaaw <ahmedtaaw@gmail.com>
2018-02-28 23:06:55 +0200

    updates

--- js/app.js
+++ js/app.js
@@ -4,22 +4,12 @@ var map;
 var marker;
 var infowindow;
 function initialize() {
-  var startmap = { lat: 29.9792971, lng: 31.1332649 }
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: startmap,
-    zoom: 6
-  });
+  
 
-
-  for (var x in landmarkslocations) {
-    var building = landmarkslocations[x];
-    var location = new google.maps.LatLng(building.lat, building.lng);
-    addMarker(map, building.name, location);
-
-  }
+ 
 }
 
-function addMarker(map, name, location) {
+function addMarker(map, name,description, location) {
   var marker = new google.maps.Marker({
     position: location,
     map: map
@@ -30,8 +20,8 @@ function addMarker(map, name, location) {
     if (typeof infowindow != 'undefined') {
       infowindow.close();
     }
-     infowindow = new google.maps.InfoWindow({
-      content: name
+    infowindow = new google.maps.InfoWindow({
+      content: name+'<br>'+description
     });
     infowindow.open(map, marker);
   });
@@ -46,37 +36,52 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 var searchTerm;
 var url;
- var wikibuilding ;
-$(function(){
+var wikibuilding;
+
+
+$(function () {
+  var startmap = { lat: 29.9792971, lng: 31.1332649 }
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: startmap,
+    zoom: 6
+  });
+
   for (var x in landmarkslocations) {
-    wikibuilding= landmarkslocations[x];
-    getwikidata(wikibuilding.name);
-   
+    wikibuilding = landmarkslocations[x];
+    getwikidata(wikibuilding.name,x);
+
     //var location = new google.maps.LatLng(building.lat, building.lng);
     //addMarker(map, building.name, location);
 
   }
-  function getwikidata(placename){
-  
+  function getwikidata(placename,indexofdata) {
+
     //searchTerm = $("#wikisearch").val();
-    url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+placename+'&format=json&callback=?';
+    url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + placename + '&format=json&callback=?';
 
     var promise = $.ajax({
-      url:url,
-      type:'GET',
-      contentType:'application/json; charset=utf-8',
-      dataType:'json',
-      success:function(data,status,jqXHR){
-       
+      url: url,
+      type: 'GET',
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      success: function (data, status, jqXHR) {
+
       }
     });
 
-    promise.then(function(resolve){
+    promise.then(function (resolve) {
       //console.log("data "+ resolve)
-      console.log('Place name '+resolve[1][0]);
-      console.log('place info '+resolve[2][0]);
+      console.log('Place name ' + resolve[1][0]);
+      console.log('place info ' + resolve[2][0]);
       console.log('-------------------');
-    })
+      $('.loadingwrap').hide();
+      
+        var building = landmarkslocations[indexofdata];
+        var location = new google.maps.LatLng(building.lat, building.lng);
+        addMarker(map, building.name,resolve[2][0], location);
     
+     
+    })
+
   };
 })
