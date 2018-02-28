0df78f95363e179f2c741de33ff82efcbe59ed73
ahmedtaaw <ahmedtaaw@gmail.com>
2018-02-28 19:15:01 +0200

    update

---
+++ js/locations.js
@@ -0,0 +1,37 @@
+var locations=[
+    {
+        'name':'The Great Pyramid at Giza',
+        'lat':'29.979303',
+        'lng':'31.1340128'
+    },
+    {
+        'name':'Valley of the Kings',
+        'lat':'25.7403244',
+        'lng':'32.6010206'
+    },
+    {
+        'name':'Bibliotheca Alexandrina',
+        'lat':'31.2090083',
+        'lng':'29.9082369'
+    },
+    {
+        'name':'St Catherine',
+        'lat':'28.5598768',
+        'lng':'33.9457757'
+    },
+    {
+        'name':'Al Hakim Mosque',
+        'lat':'30.0546049',
+        'lng':'31.2635608'
+    },
+    {
+        'name':'Philae Temple',
+        'lat':'24.0252419',
+        'lng':'32.8837573'
+    },
+    {
+        'name':'Wadi el Gemal National Park',
+        'lat':'24.4746349',
+        'lng':'35.098064'
+    }
+]
